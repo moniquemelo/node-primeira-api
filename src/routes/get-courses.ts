c1709ef3fa03f2d1aea906592d474b17e166d56d
moniqmelo <jmoniquemelo@gmail.com>
2025-10-09 18:50:53 -0300

    Implementação filter, orderby, paginação

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { db } from '../database/client.ts'
 import { courses } from '../database/schema.ts'
 import z from 'zod'
+import { ilike, asc, and, SQL } from 'drizzle-orm'
 
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
@@ -10,6 +11,11 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
         schema: {
             tags: ['Courses'],
             summary: 'Lista todos os cursos',
+            querystring: z.object({
+                search: z.string().optional(),
+                orderBy: z.enum(['id', 'title']).optional().default('id'),
+                page: z.coerce.number().optional().default(1),
+            }),
             description: 'Essa rota lista todos os cursos cadastrados no banco de dados.',
             response: {
                 200: z.object({
@@ -18,21 +24,41 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
                             id: z.uuid(),
                             title: z.string(),
                         })
-                    )
+                    ),
+                    total: z.number(),
                 }).describe('Lista de cursos retornada com sucesso!'),
             }
         }
 
     }, async (request, reply) => {
+
+        const { search, orderBy, page } = request.query
+
+        const conditions: SQL[] = []
+
+        if (search) {
+            conditions.push(ilike(courses.title, `%${search}%`))
+        }
+
+        const [result, total] = await Promise.all([
+
+            db.select({
+                id: courses.id,
+                title: courses.title,
+            })
+                .from(courses)
+                .orderBy(asc(courses[orderBy]))
+                .offset((page - 1) * 2) // Paginação, pular 2 cursos por página
+                .limit(2)  // Paginação, limitar a 2 cursos por página
+                .where(and(...conditions)), // Filtro para buscar cursos pelo título
+
+            db.$count(courses, and(...conditions))
+
+        ])
         //const result = await db.select().from(courses)
 
-        // Caso eu quisesse selecionar apenas alguns campos
-        const result = await db.select({
-            id: courses.id,
-            title: courses.title,
-        }).from(courses)
 
-        return reply.send({ courses: result })
+        return reply.send({ courses: result, total })
     })
 
 }
